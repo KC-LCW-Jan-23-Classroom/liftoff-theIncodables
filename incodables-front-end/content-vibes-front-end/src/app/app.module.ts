662f897a0f277b2dcadfa982c5432ed8d5f614d6
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-10-10 19:37:25 -0500

    had to fix the app.module.ts

--- incodables-front-end/content-vibes-front-end/src/app/app.module.ts
+++ incodables-front-end/content-vibes-front-end/src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    RegistrationFormComponent,
     RegistrationComponent
   ],
   imports: [

4d43b65e63c35161978c9ebe34dbd305c4f2128f
jojopenguin191 <jojopenguin191@gmail.com>
2023-10-30 18:51:40 -0500

    Copied html to user-landing-page, deleted some stuff

--- incodables-front-end/content-vibes-front-end/src/app/user-landing-page/user-landing-page.component.ts
+++ incodables-front-end/content-vibes-front-end/src/app/user-landing-page/user-landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-landing-page',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLandingPageComponent implements OnInit {
   sessions: any[] = [{ name: 'test', date: '50/69/78' }, { name: 'test', date: '50/69/78' }, { name: 'test', date: '50/69/78' }];
+  @Input() isExpanded: boolean = false;
+  @Output() toggleSidebar: EventEmitter<boolean> = new 
+  EventEmitter<boolean>();
 
   constructor() {}
 

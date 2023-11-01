8e7d5447e9ac0c23bb07b2c34099db3c7a6415e9
Tamara Leonard <LeonardTamaraD@gmail.com>
2023-11-01 14:12:21 -0500

    "Epic 3 Issue 4 Get request"

--- incodables-front-end/content-vibes-front-end/src/app/user-landing-page/user-landing-page.component.ts
+++ incodables-front-end/content-vibes-front-end/src/app/user-landing-page/user-landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-landing-page',
@@ -6,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-landing-page.component.css'],
 })
 export class UserLandingPageComponent implements OnInit {
-  sessions: any[] = [{ name: 'test', date: '50/69/78' }, { name: 'test', date: '50/69/78' }, { name: 'test', date: '50/69/78' }];
+  sessions: any[] = [/*{ name: 'test', date: '50/69/78' }, { name: 'test', date: '50/69/78' }, { name: 'test', date: '50/69/78' }*/];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getSessions().subscribe((sessions: any[]) => {
+      this.sessions = sessions;
+    });
+  }
+
+  getSessions(): Observable<any[]> {
+    return this.http.get<any[]>('http://localhost:8080/campaigns/all');
+  }
 }

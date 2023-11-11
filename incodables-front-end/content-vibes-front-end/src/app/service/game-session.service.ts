4fc42b8d8b719f4a323546f6db400be3f04fbe85
AJT <lunegeoid@gmail.com>
2023-11-11 15:01:24 -0600

    began writing logic for game-session service by refactoring campaign service

--- incodables-front-end/content-vibes-front-end/src/app/service/game-session.service.ts
+++ incodables-front-end/content-vibes-front-end/src/app/service/game-session.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+// import { Campaign } from '../model/campaign';
+// import { CampaignDTO } from '../model/campaign-dto'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameSessionService {
+  private baseUrl = 'http://localhost:8080/game'; // Update with your Spring Boot backend URL
+  userService: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createGameSession(gameSessionDTO: gameSessionDTO): Observable<gameSession> {
+    console.log('game session:', gameSessionDTO);
+    return this.http.post<GameSession>(
+      `${this.baseUrl}/create/{campaignId}`,
+      gameSessionDTO,
+      { withCredentials: true }
+    );
+  }
+
+  // getAllCampaigns(): Observable<Campaign[]> {
+
+  //   return this.http.get<Campaign[]>(`${this.baseUrl}/campaigns/all`,{ withCredentials: true });
+  // }
+
+  // getCampaignById(campaignId: number): Observable<Campaign> {
+  //   return this.http.get<Campaign>(`${this.baseUrl}/campaigns/${campaignId}`);
+  // }
+
+  // deleteCampaign(campaignId: number): Observable<void> {
+  //   return this.http.delete<void>(`${this.baseUrl}/campaigns/delete/${campaignId}`);
+  // }
 }
+
+

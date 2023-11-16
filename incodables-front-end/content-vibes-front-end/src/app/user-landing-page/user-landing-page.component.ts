45f1b2abe3ae64b5d238f9510b3081b31db81cad
AJT <lunegeoid@gmail.com>
2023-11-16 16:41:09 -0600

    post merge updates

--- incodables-front-end/content-vibes-front-end/src/app/user-landing-page/user-landing-page.component.ts
+++ incodables-front-end/content-vibes-front-end/src/app/user-landing-page/user-landing-page.component.ts
@@ -1,28 +1,21 @@
-import { Component, OnInit,  Inject } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../service/user-service/user.service';
 import { CampaignService } from '../service/campaign.service';
 import { Campaign } from '../model/campaign';
 import { MusicSelectionComponent } from '../music-selection/music-selection.component';
-import { GameSessions } from '../model/gamesession-model';
-
-
-
-
 
 @Component({
   selector: 'app-user-landing-page',
   templateUrl: './user-landing-page.component.html',
   styleUrls: ['./user-landing-page.component.css'],
 })
-
 export class UserLandingPageComponent implements OnInit {
   username: string | null;
   campaigns: Campaign[] = [];
   selectedCampaignId: number | null = null;
-  selectedSession: GameSessions | undefined;
+  // selectedSession: GameSessions | undefined;
 
   constructor(
     private route: ActivatedRoute,
@@ -30,21 +23,21 @@ export class UserLandingPageComponent implements OnInit {
     private http: HttpClient,
     private userService: UserService,
     private campaignService: CampaignService,
-    @Inject(MusicSelectionComponent) private musicSelectionComponent: MusicSelectionComponent) {
+    @Inject(MusicSelectionComponent)
+    private musicSelectionComponent: MusicSelectionComponent
+  ) {
     this.username = this.userService.getUserInfo();
-
   }
 
   ngOnInit(): void {
     this.campaignService.getAllCampaigns().subscribe((campaigns: any[]) => {
-      
       this.campaigns = campaigns;
       console.log(campaigns);
     });
     console.log('campaigns: ', this.campaigns);
   }
 
-  setSelectedGameSession(session:any){
+  setSelectedGameSession(session: any) {
     this.setSelectedGameSession = session;
   }
 
@@ -58,6 +51,4 @@ export class UserLandingPageComponent implements OnInit {
       'Track 3',
     ];
   }
-
-
 }

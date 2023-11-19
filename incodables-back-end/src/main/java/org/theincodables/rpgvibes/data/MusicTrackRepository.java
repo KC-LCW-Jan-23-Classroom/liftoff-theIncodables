818e8a8452503d43ba947a5bab0ea905c6884ca8
AJT <lunegeoid@gmail.com>
2023-11-19 12:15:29 -0600

    added MusicTrackRepository

---
+++ incodables-back-end/src/main/java/org/theincodables/rpgvibes/data/MusicTrackRepository.java
@@ -0,0 +1,11 @@
+package org.theincodables.rpgvibes.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.theincodables.rpgvibes.models.MusicTracks;
+
+import java.util.List;
+@Repository
+public interface MusicTrackRepository extends CrudRepository <MusicTracks, Integer> {
+    List<MusicTracks> findByGameSessionId(Integer gameSessionId);
+}

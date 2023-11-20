e5a8c51de061f4fa282efd539fe41a8c4d29f165
Tamara Leonard <LeonardTamaraD@gmail.com>
2023-11-20 12:03:52 -0600

    "epic-5-issue-23"

--- incodables-front-end/content-vibes-front-end/src/app/music-selection/music-selection.component.ts
+++ incodables-front-end/content-vibes-front-end/src/app/music-selection/music-selection.component.ts
@@ -3,10 +3,8 @@ import { GameSessions } from '../model/gamesession-model';
 
 export interface TrackPreview {
   id: string;
-  title: string;
-  artist: string;
-  album: string;
-  previewUrl: string;
+  url: string;
+  name: string;
 }
 
 @Component({
@@ -14,7 +12,23 @@ export interface TrackPreview {
   templateUrl: './music-selection.component.html',
   styleUrls: ['./music-selection.component.css']
 })
+
 export class MusicSelectionComponent implements OnInit {
+  musicTracks: TrackPreview[] = [
+    { id: '709397', url: 'https://freesound.org/people/guitarman213/sounds/709397/', name: 'Fantasy Cinematic Music Four' },
+    { id: '698282', url: 'https://freesound.org/people/TheoJT/sounds/698282/', name: 'Peaceful Fantasy Music' },
+    { id: '511311', url: 'https://freesound.org/people/TheoJT/sounds/511311/', name: 'Fantasy Classical Themes' },
+    { id: '595715', url: 'https://freesound.org/people/szegvari/sounds/595715/', name: 'Haunted Hall - Cinematic Fantasy Music Thriller Orchestra atmo Amb soundscape.wav' },
+    { id: '595853', url: 'https://freesound.org/people/szegvari/sounds/595853/', name: 'Old ritual - Fantasy Background Soundscape Haunted Atmo Music Synth Drum Orchestra - Mastered.wav' },
+    { id: '614092', url: 'https://freesound.org/people/szegvari/sounds/614092/', name: 'Dark Atmo Sea Beach Sad Mood Myst Thriller Fantasy.wav' },
+    { id: '489035', url: 'https://freesound.org/people/Michael-DB/sounds/489035/', name: 'Game-Music-01' },
+    { id: '624018', url: 'https://freesound.org/people/Michael-DB/sounds/624018/', name: 'Cinematic Music-04' },
+    { id: '569401', url: 'https://freesound.org/people/Michael-DB/sounds/569401/', name: 'Cinematic Music - Relinquish' },
+    { id: '569400', url: 'https://freesound.org/people/Michael-DB/sounds/569400/', name: 'Cinematic Music - Judgements' },
+    { id: '416632', url: 'https://freesound.org/people/Sirkoto51/sounds/416632/', name: 'Castle Music Loop #1' },
+    { id: '609900', url: 'https://freesound.org/people/szegvari/sounds/609900/', name: 'Slow Sea Orchestra Big Orchestral String Piano Myst Drama Love Cinematic Music Surround.wav' }
+  ];
+  
   selectedTracks: string[] = [];
   selectedTrackObjects: TrackPreview[] = [];
 
@@ -25,37 +39,28 @@ export class MusicSelectionComponent implements OnInit {
   
   constructor() {
     this.selectedSession = new GameSessions();
-    
-    this.selectedSession.subscribe((session) => {
-      const trackPreviewAudioElement = document.getElementById('track-preview-audio');
-      if (trackPreviewAudioElement) {
-        const htmlAudioElement = trackPreviewAudioElement as HTMLAudioElement;
-        htmlAudioElement.src = session.trackPreviewUrl;
-      }
-    });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
-  addTrack() {
-    if (this.track && typeof this.track === 'object' && this.track.hasOwnProperty('id')) {
-      this.selectedTracks.push(this.track.id);
-      this.selectedTrackObjects.push(this.track);
+  addTrack(track: TrackPreview) {
+    if (track && typeof track === 'object' && track.hasOwnProperty('id')) {
+      this.selectedTracks.push(track.id);
+      this.selectedTrackObjects.push(track);
   
       if (this.selectedSession != undefined) {
-        const trackIdToAdd = this.track.id;
+        const trackIdToAdd = track.id;
         this.selectedSession.tracks.push(trackIdToAdd);
       }
     }
   }
 
   playTrackPreview() {
-    // Get the audio element for the track preview.
-    const audioElement = document.getElementById('track-preview-audio');
+      const audioElement = document.getElementById('track-preview-audio');
 
-    // If the audio element exists, play it.
-    if (audioElement instanceof HTMLAudioElement) {
-      audioElement.play();
-    }
+      if (audioElement instanceof HTMLAudioElement && this.track) {
+        audioElement.src = this.track.url;
+        audioElement.play();
+      } 
   }
 }
